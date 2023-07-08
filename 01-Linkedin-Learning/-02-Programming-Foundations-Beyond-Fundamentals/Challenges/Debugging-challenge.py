2e30cdf3487753e0e7febe97f25c5a4766ea99a0
Mustafa Eisa <116859259+Mustafa-isa@users.noreply.github.com>
2023-07-08 13:53:48 +0300

    Update Debugging-challenge.py

--- 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/Debugging-challenge.py
+++ 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/Debugging-challenge.py
@@ -1,1 +1,12 @@
+def plant_recommendation(care):
+    if care == 'low':
+        print('alone')
+    elif care == 'medium':
+        print('pothos')
+    elif care == 'high':
+        print('orchid')
 
+# Here the is an error
+plant_recommendation('low')
+plant_recommendation('medium')
+plant_recommendation('high')
