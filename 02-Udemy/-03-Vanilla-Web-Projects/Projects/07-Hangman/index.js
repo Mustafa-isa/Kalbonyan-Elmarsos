755ae970b3ee82fc764f54e22b94c1ffc238219a
Mustafa Eisa <116859259+Mustafa-isa@users.noreply.github.com>
2023-07-08 16:01:57 +0300

    Add files via upload

---
+++ 02-Udemy/-03-Vanilla-Web-Projects/Projects/07-Hangman/index.js
@@ -0,0 +1,14 @@
+let figure =document.querySelectorAll(".hidden")
+let wrongLetters =document.querySelector(".wrongLetters")
+let wordel =document.querySelector(".word")
+
+let popupParent =document.querySelector(".popup")
+let poo =document.querySelector(".pop")
+let finalMessage = document.querySelector(".final-meesage ")
+let noti =document.querySelector(".notifavtion-conatiner")
+/// code
+let words =['photo' ,'ship' ,'programming']
+let slected = words[ Math.floor(Math.random() * words.length)]
+
+
+console.log(slected)
